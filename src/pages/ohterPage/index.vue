bd426df0380ea77bd834aa005c31e779bdb78ed2
zhangy <798194200@qq.com>
2020-12-03 13:04:49 +0800

    测试svg 标签

--- src/pages/ohterPage/index.vue
+++ src/pages/ohterPage/index.vue
@@ -1,54 +1,71 @@
 <template>
-    <div>
-        <el-tabs type="border-card">
-            <el-tab-pane :lazy="true">
-                <span slot="label"><i class="el-icon-document"></i> HTML</span>
-                <MyEditor
-                        :language="'json'"
-                        :codes="htmlCodes"
-                        @onMounted="htmlOnMounted"
-                        @onCodeChange="htmlOnCodeChange" />
-            </el-tab-pane>
-        </el-tabs>
-    </div>
+  <div>
+    <el-tabs type="border-card">
+      <el-tab-pane :lazy="true">
+        <span slot="label"><i class="el-icon-document"></i> HTML</span>
+        <MyEditor
+            :language="'json'"
+            :codes="htmlCodes"
+            @onMounted="htmlOnMounted"
+            @onCodeChange="htmlOnCodeChange"/>
+      </el-tab-pane>
+      <el-tab-pane :lazy="true">
+        <span slot="label"> svg</span>
+        <svg width="400" height="400">
+          <rect class="rect" x="10" y="10" width="300" height="300"></rect>
+          <text class="text" x="20" y="50">hello world</text>
+        </svg>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <script>
-    import MyEditor from './myEditor'
-    export default {
-        components:{
-            MyEditor
-        },
-        data () {
-            return {
-                htmlCodes:'[{"name":"name"},{"name":"name"},{"name":"name"},{"name":"name"}]',
-                javascriptCodes:'console.log("This is javascript")',
-                jsonCodes:'console.log("This is javascript")',
-                cssCodes:'body{}',
-                htmlEditor:null,
-                jsEditor:null,
-                cssEditor:null,
-                jsonEditor:[{"name":"name"},{"name":"name"},{"name":"name"},{"name":"name"}],
-            }
-        },
-        methods:{
-            htmlOnMounted(edit){
-                this.htmlEditor = edit;
-            },
-            javascriptOnMounted(edit){
-                this.jsEditor = edit;
-            },
-            cssOnMounted(edit){
-                this.cssEditor = edit;
-            },
-            htmlOnCodeChange(){},
-            // javascriptOnCodeChange(value,event){},
-            // cssOnCodeChange(value,event){},
-        }
+import MyEditor from './myEditor'
+
+export default {
+  components: {
+    MyEditor
+  },
+  data() {
+    return {
+      htmlCodes: '[{"name":"name"},{"name":"name"},{"name":"name"},{"name":"name"}]',
+      javascriptCodes: 'console.log("This is javascript")',
+      jsonCodes: 'console.log("This is javascript")',
+      cssCodes: 'body{}',
+      htmlEditor: null,
+      jsEditor: null,
+      cssEditor: null,
+      jsonEditor: [{"name": "name"}, {"name": "name"}, {"name": "name"}, {"name": "name"}],
     }
+  },
+  methods: {
+    htmlOnMounted(edit) {
+      this.htmlEditor = edit;
+    },
+    javascriptOnMounted(edit) {
+      this.jsEditor = edit;
+    },
+    cssOnMounted(edit) {
+      this.cssEditor = edit;
+    },
+    htmlOnCodeChange() {
+    },
+    // javascriptOnCodeChange(value,event){},
+    // cssOnCodeChange(value,event){},
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style scoped lang="less">
+.rect {
+  fill: red;
+}
+.text{
+  fill:none;
+  stroke:blue;
+  stroke-width: 2px;
+  font-size: 60px ;
+}
 </style>
